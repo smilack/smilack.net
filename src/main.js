38f0d406d66c240cf60a0bc94ee1bf261f1a9b61
smilack <tom@smilack.net>
2017-07-21 20:08:29 -0500

    Fix flickering hover hightlight

--- src/main.js
+++ src/main.js
@@ -4,28 +4,14 @@ function init() {
    var wedges = Object.values(document.getElementsByClassName("wedge"));
 
    wedges.map(wedge => wedge.addEventListener("mouseenter", onElementMouseEnter));
-   wedges.map(wedge => wedge.addEventListener("mouseleave", onElementMouseLeave));
 }
 
 function onElementMouseEnter(event) {
    var wedge = event.target,
        use = getUse(wedge);
    
-   console.log(`mouseenter ${event.target.tagName}#${event.target.id}.${Array.from(event.target.classList).join(".")}`);
-
    if(use !== null) {
       use.setAttribute("href", "#" + wedge.id);
-   }
-}
-
-function onElementMouseLeave(event) {
-   var wedge = event.target,
-       use = getUse(wedge);
-
-   console.log(`mouseleave ${event.target.tagName}#${event.target.id}.${Array.from(event.target.classList).join(".")}`);
-
-   if(use !== null) {
-      use.setAttribute("href", "#");
    }
 }
 
